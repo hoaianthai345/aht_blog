d3a06acfdcc9b5ff8ca4a158cd73bd1b4ad40739
AnHoaiThai <hoaianthai345@gmail.com>
2025-11-21 16:26:46 +0700

    Quartz sync: Nov 21, 2025, 4:26 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -43,9 +43,7 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph({
-
-    }),
+    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
